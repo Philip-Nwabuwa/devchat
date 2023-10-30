b874c2009c5195e562bc9faf8f0777e8852261cb
Philip John <95682231+Philip-Nwabuwa@users.noreply.github.com>
2023-10-30 18:26:42 +0000

    Main Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,21 @@
-"use client";
-
-import { useSession } from "next-auth/react";
+import { getServerSession } from "next-auth";
 import Link from "next/link";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
-const Page = () => {
-  const { data: session } = useSession();
-  if (session) {
-    return (
-      <div>
-        <h1>Hi {session.user?.name}</h1>
-        <p>Heres your email: {session?.user?.email}</p>
-        wish to countinue? <Link href="/dashboard">Dashboard</Link>
-      </div>
-    );
+const page = async () => {
+  const session = await getServerSession(authOptions)
+  
+  if (session?.user?.email) {
+    redirect("/dashboard");
   }
-  return <div>page</div>;
+  return (
+    <div>
+      <h1>Welcome to Dev Chat</h1>
+      <p>Please Login</p>
+      <Link href="/login">Login</Link>
+    </div>
+  );
 };
 
-export default Page;
+export default page;
