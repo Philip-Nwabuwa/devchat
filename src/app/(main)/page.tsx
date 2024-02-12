13bcef4a7c262276c84000df771a33479fbffe99
Philip John <95682231+Philip-Nwabuwa@users.noreply.github.com>
2024-02-12 10:35:25 +0000

    chore: Added clerk auth with middleware.

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,3 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <main>This is a protected route</main>;
+  return (
+    <main>
+      This is a protected route{" "}
+      <div>
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </main>
+  );
 }
